aa8699d198b13c308cc6c3b695de3ae44e285a8e
Kyle Fuller <inbox@kylefuller.co.uk>
2012-02-29 20:58:53 +0000

    Fix sed

--- bot/api.py
+++ bot/api.py
@@ -1,2 +1,3 @@
 from bot.decorators import command, regex, private, opped
+from bot.events import Event
 from bot.module import Module
--- sed.py
+++ sed.py
@@ -10,7 +10,7 @@ class sed(Module):
     def OnChanMsg(self, nick, chan, message):
         match = SUBSTITUTE_REGEX.match(str(message))
         if match and str(nick) in self.previous_lines:
-            event = bot.Event(module=self, nick=str(nick), channel=str(chan), line=str(self.previous_lines[str(nick)]))
+            event = Event(module=self, nick=str(nick), channel=str(chan), line=str(self.previous_lines[str(nick)]))
             self.substitute(event, string=self.previous_lines[str(nick)], **match.groupdict())
         else:
             self.previous_lines[str(nick)] = str(message)
@@ -21,5 +21,5 @@ class sed(Module):
         self.substitute(event, string=line, **SUBSTITUTE_REGEX.match(sed).groupdict())
 
     def substitute(self, event, *args, **kwargs):
-        event.reply('{} meant "{}"'.format(event['nick'], re.sub(*args, **kwargs)))
+        event.write('{} meant "{}"'.format(event['nick'], re.sub(*args, **kwargs)))
 
