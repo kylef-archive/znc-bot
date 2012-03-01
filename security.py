aab7ce2ddd3eeff9c3f2363efcd5e8a221be2952
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-01 22:36:36 +0000

    Make security use stdin

--- security.py
+++ security.py
@@ -4,7 +4,7 @@ from bot.api import *
 
 class security(Module):
     @command(usage='[hash data]', example='sha256 hello world')
-    @regex(r'^((?P<algorithm>\S+) (?P<data>.+))?$')
+    @regex(r'^((?P<algorithm>\S+)(?P<data>.+)?)?$')
     def hash(self, event, algorithm=None, data=''):
         if not algorithm:
             if hasattr(hashlib, 'algorithms_available'):
@@ -14,20 +14,30 @@ class security(Module):
 
             return ', '.join(algorithms)
 
+        if event.stdin:
+            data = event.stdin
+
         try:
             return hashlib.new(algorithm, bytes(data, 'utf8')).hexdigest()
         except ValueError:
             return '{}: Unknown algorithm'.format(algorithm)
 
     @command(description='Encode the input with base64')
-    def base64(self, event, line):
-        return str(base64.encodestring(bytes(line, 'utf8')), 'utf8')
+    def base64(self, event, data):
+        if event.stdin:
+            data = event.stdin
+        return str(base64.encodestring(bytes(data, 'utf8')), 'utf8')
 
     @command(description='Decode the input with base64')
-    def decode64(self, event, line):
-        return str(base64.decodestring(bytes(line, 'utf8')), 'utf8')
+    def decode64(self, event, data):
+        if event.stdin:
+            data = event.stdin
+        return str(base64.decodestring(bytes(data, 'utf8')), 'utf8')
 
     @command
-    def rot13(self, event, line):
+    def rot13(self, event, data):
+        if event.stdin:
+            data = event.stdin
+
         rot13_trans = str.maketrans('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz', 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm')
-        return line.translate(rot13_trans)
+        return data.translate(rot13_trans)
