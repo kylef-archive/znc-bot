c026a5f539e17bd13d9f5301fbf230bc6e932d3c
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-26 21:37:00 +0100

    Add a urban dictionary command

---
+++ urban.py
@@ -0,0 +1,21 @@
+from bot.api import *
+
+class urban(Module):
+    @command(name='urban', description='Search urban dictionary')
+    @http
+    def search(self, event, query):
+        event.http('http://www.urbandictionary.com/iphone/search/define', qs={'term':query})
+
+    @search.http(200)
+    def handle_search(self, event, response):
+        result = response.json
+
+        if 'list' in result:
+            if 'definition' in result['list'][0]:
+                definition = result['list'][0]['definition'].replace('\r\n', '')
+                if len(definition) > 400:
+                    return definition[:400] + '...'
+                return definition
+
+        return 'definition not found'
+
