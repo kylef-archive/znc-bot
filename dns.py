9d367b33219d44fa0c8623455361a29a9fd74d59
Kyle Fuller <inbox@kylefuller.co.uk>
2012-03-29 00:07:53 +0100

    dns: add domain lookups

--- dns.py
+++ dns.py
@@ -7,3 +7,12 @@ class dns(Module):
         result = socket.gethostbyaddr(line)
         return '{0} ({1})'.format(result[0], result[2][0])
 
+    @command
+    @http
+    def domain(self, event, line):
+        event.http('https://dnsimple.com/domains/{}/check'.format(line), headers={'Accept':'application/json'})
+
+    @domain.http(200)
+    def domain_check(self, event, response):
+        return '{name} ({status})'.format(**response.json)
+
