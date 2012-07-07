0376be8f4071feae3008d94fc51107397105403b
Kyle Fuller <inbox@kylefuller.co.uk>
2012-07-07 19:36:04 +0100

    dns: Add geoip lookup

--- dns.py
+++ dns.py
@@ -16,3 +16,12 @@ class dns(Module):
     def domain_check(self, event, response):
         return '{name} ({status})'.format(**response.json)
 
+    @command
+    @http
+    def geoip(self, event, line):
+        event.http('http://freegeoip.net/json/{}'.format(line))
+
+    @geoip.http(200)
+    def geoip_response(self, event, response):
+        return '{city}, {country_name}'.format(**response.json)
+
